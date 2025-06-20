2d256932f1ea3af4902659362c9380b689af2d89
FrAgGeR0101 <daniilkarabut@gmail.com>
2025-06-20 06:38:54 +0200

    Update MixinSignOpen.java

--- src/main/java/com/github/mkram17/bazaarutils/mixin/MixinSignOpen.java
+++ src/main/java/com/github/mkram17/bazaarutils/mixin/MixinSignOpen.java
@@ -2,27 +2,25 @@ package com.github.mkram17.bazaarutils.mixin;
 
 import com.github.mkram17.bazaarutils.BazaarUtils;
 import com.github.mkram17.bazaarutils.events.SignOpenEvent;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ingame.SignEditScreen;
+import net.minecraft.client.gui.GuiEditSign;
+import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//TODO maybe try to do this with AbstractSignEditScreen init() and see if it works to be more efficient
-//used for SignOpenEvent
-@Mixin(Screen.class)
-public class MixinSignOpen {
-    @Inject(method = "init", at = @At("HEAD"))
-    private void onScreenInit(CallbackInfo ci) {
-        Screen screen = (Screen) (Object) this;
+/**
+ * Fires a {@link SignOpenEvent} whenever the vanilla sign-editing GUI
+ * ({@link GuiEditSign}) is opened.  Uses 1.8.9 classes only.
+ */
+@Mixin(GuiScreen.class)
+public abstract class MixinSignOpen {
 
-        if (screen instanceof SignEditScreen) {
-            // Post the SignOpenEvent
-            SignOpenEvent event = new SignOpenEvent((SignEditScreen) screen);
-            BazaarUtils.eventBus.post(event);
-//            Util.notifyAll("Sign Open Event posted!");
-
+    /** `GuiScreen.initGui()` is called once when the screen opens. */
+    @Inject(method = "initGui", at = @At("HEAD"))
+    private void bazaarutils$onInitGui(CallbackInfo ci) {
+        if ((Object) this instanceof GuiEditSign sign) {
+            BazaarUtils.EVENT_BUS.post(new SignOpenEvent(sign));
         }
     }
 }
