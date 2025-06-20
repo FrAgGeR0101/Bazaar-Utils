bdb7b3a877597af20d37c50b4c0f800b9182dc42
FrAgGeR0101 <daniilkarabut@gmail.com>
2025-06-20 06:36:29 +0200

    Update AccessorScreen.java

--- src/main/java/com/github/mkram17/bazaarutils/mixin/AccessorScreen.java
+++ src/main/java/com/github/mkram17/bazaarutils/mixin/AccessorScreen.java
@@ -1,13 +1,18 @@
 package com.github.mkram17.bazaarutils.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-//used for SlotClickEvent
-@Mixin(Screen.class)
-public interface AccessorScreen {
-    @Accessor("client")
-    MinecraftClient getClient();
+/**
+ * Simple accessor that exposes the protected {@code mc} field of {@link GuiScreen}.
+ * Only needed by legacy helpers; no Fabric classes involved.
+ */
+@Mixin(GuiScreen.class)
+public interface AccessorGuiScreen {
+
+    /** Vanilla 1.8.9 field name is {@code mc}. */
+    @Accessor("mc")
+    Minecraft getMinecraft();
 }
