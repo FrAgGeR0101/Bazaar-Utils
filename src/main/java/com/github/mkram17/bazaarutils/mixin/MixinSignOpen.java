0caa4468f2d76704864ca2400d67b2bca4851e04
FrAgGeR0101 <daniilkarabut@gmail.com>
2025-06-20 06:44:53 +0200

    Update MixinSignOpen.java

--- src/main/java/com/github/mkram17/bazaarutils/mixin/MixinSignOpen.java
+++ src/main/java/com/github/mkram17/bazaarutils/mixin/MixinSignOpen.java
@@ -1,25 +1,10 @@
-package com.github.mkram17.bazaarutils.mixin;
-
-import com.github.mkram17.bazaarutils.BazaarUtils;
-import com.github.mkram17.bazaarutils.events.SignOpenEvent;
-import net.minecraft.client.gui.GuiEditSign;
-import net.minecraft.client.gui.GuiScreen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-/**
- * Fires a {@link SignOpenEvent} whenever the vanilla sign-editing GUI
- * ({@link GuiEditSign}) is opened.  Uses 1.8.9 classes only.
- */
 @Mixin(GuiScreen.class)
 public abstract class MixinSignOpen {
 
-    /** `GuiScreen.initGui()` is called once when the screen opens. */
     @Inject(method = "initGui", at = @At("HEAD"))
     private void bazaarutils$onInitGui(CallbackInfo ci) {
-        if ((Object) this instanceof GuiEditSign sign) {
+        if (this instanceof GuiEditSign) {
+            GuiEditSign sign = (GuiEditSign) (Object) this;  // classic cast
             BazaarUtils.EVENT_BUS.post(new SignOpenEvent(sign));
         }
     }
